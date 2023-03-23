18bbf5391d57f39f7480c16a5cbcbb0afa329565
Mdlalose4life <blessing1059@gmail.com>
2023-03-23 02:42:47 +0200

    Fixing bugs

--- models/place.py
+++ models/place.py
@@ -2,14 +2,12 @@
 #!/usr/bin/python3
 """This is the place class"""
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
+from models.base_model import BaseModel, Base, Amenity
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 import models
 import shlex
-
 
 place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60),
