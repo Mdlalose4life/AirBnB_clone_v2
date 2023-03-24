a136ef7e7fe8ccff3dd3262f83a848742fe84acb
Mdlalose4life <blessing1059@gmail.com>
2023-03-24 03:06:05 +0200

    Editing file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
@@ -57,16 +56,16 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-    
+
     def delete(self, obj=None):
         """Deletes an existing element"""
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
-    
+
     def close(self):
         """Calls the reload() method"""
         self.reload()
