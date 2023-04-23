a696e414e64e9db59a4f803a8d1f9532e26f7521
Mdlalose4life <blessing1059@gmail.com>
2023-04-23 22:16:37 +0200

    The older method

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -54,7 +54,7 @@ def number_template(n):
 
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def odd_or_even(n):
+def number_odd_or_even(n):
     """Displays HTML and an n number."""
     return render_template('6-number_odd_or_even.py', n=n)
 
