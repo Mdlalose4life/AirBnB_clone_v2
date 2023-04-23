42f03a0a1f8d1b5786be71504a029c754a53a4a4
Mdlalose4life <blessing1059@gmail.com>
2023-04-23 15:06:33 -0700

    The reload

--- models/city.py
+++ models/city.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
+from models import storage
 
 
 class City(BaseModel, Base):
@@ -15,7 +16,22 @@ class City(BaseModel, Base):
         name: input name
     """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    if storage_type == 'db':
+	name = Column(String(128), nullable=False)
+	state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+	places = relationship("Place", cascade='all, delete, delete-orphan',
+				backref="cities")
+    else:
+	name=''
+	
+
+	@property
+	def cities(self):
+	''' 
+	'''
+	related_cities = []
+	cities = storage.all(City)
+	for city in cities.values():
+	    if city.state_id == self.id:
+		related_cities.append(city)
+	return related_cities
