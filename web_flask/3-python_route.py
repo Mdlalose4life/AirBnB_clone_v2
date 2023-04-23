40b84fbe7252573c8253e6610191dd460994ecdc
Mdlalose4life <blessing1059@gmail.com>
2023-04-23 11:59:26 +0200

    Displat python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+script that starts a Web application
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ returns Hello HBNB """
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """ returns HBNB """
+    return 'HBNB'
+
+
+@app.route("/c/<text>")
+def text(text):
+    """
+    Displays C then
+    Retuns the given string, and replaces underscores with
+    a space
+    """
+    string = "C {}".format(text.replace("_", " "))
+    return string
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python(text='is cool'):
+    """
+    Displays Python then returns the given text then
+    replaces underscores with a space
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run()
