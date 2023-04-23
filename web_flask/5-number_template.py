82ffe9ecb91304158c127f5805ad35311af499f4
Mdlalose4life <blessing1059@gmail.com>
2023-04-23 15:06:05 +0200

    The n

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -53,7 +53,7 @@ def number_template(n):
     """
     Render template with a number.
     """
-    return render_template('5-number.html', number=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
