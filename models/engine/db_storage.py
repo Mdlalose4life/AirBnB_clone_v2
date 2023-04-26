171d80bb2194c6b99006cef5619dcbb124307f8c
Mdlalose4life <blessing1059@gmail.com>
2023-04-27 00:47:50 +0200

    States

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -37,22 +37,22 @@ class DBStorage:
         Return:
             returns a dictionary of __object
         """
-        dic = {}
+        dict = {}
         if cls:
             if type(cls) is str:
                 cls = eval(cls)
             query = self.__session.query(cls)
             for elem in query:
                 key = "{}.{}".format(type(elem).__name__, elem.id)
-                dic[key] = elem
+                dict[key] = elem
         else:
             lista = [State, City, User, Place, Review, Amenity]
             for clase in lista:
                 query = self.__session.query(clase)
                 for elem in query:
                     key = "{}.{}".format(type(elem).__name__, elem.id)
-                    dic[key] = elem
-        return (dic)
+                    dict[key] = elem
+        return (dict)
 
     def new(self, obj):
         """add a new element in the table
